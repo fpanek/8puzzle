c9cdc82ad7b7048bfc665a3d431301aa457c8c8b
Florian Panek <florian.panek@outlook.com>
2023-01-01 10:09:04 +0100

    *)simplified generation of random field *)0 is now not always in the field 0/0

--- eight_puzzle/create_puzzle.py
+++ eight_puzzle/create_puzzle.py
@@ -6,32 +6,23 @@
 from random import choice
 from eight_puzzle.puzzle_functions import print_puzzle
 
-min_value = 1
+min_value = 0
 max_value = 8
-number_of_values = 8
+number_of_values = 9
 debug = 1 # set to 1 and use in functions for additional debug outputs
 def create_random_puzzle():
     puzzle = [ [0,0,0], [0,0,0], [0,0,0]]
-    values = create_random_puzzle_values()    #create 8 random unique values
+    values = create_random_puzzle_values()    #create 9 random unique values
     #now its time to fill  the retrieved 8 values into the 2d array - position 0/0 stays always empty and will me omitted
     count = 0
     for x in range(3):
         for y in range(3):
-            #print("counter", count)
-            #puzzle[x][y] == values[count]
-            if (x == 0 ) and (y == 0):
-                puzzle [x][y] == 0
-            else:
-                puzzle [x][y] = values [count - 1] # puzzle has 9 slots / only 8 random values do exist
-                #todo rewrite to generate the first random number with 0 and sijplyf this functionality
-                # todo 0 must be included into random generation anyway!!!
-            #    print (count)
+            puzzle[x][y] = values[count]
             count = count + 1
 
 
     if debug:
         print_puzzle(puzzle)
-
 
 
 def gen_random_number(min, max, exclude):
@@ -40,7 +31,8 @@ def gen_random_number(min, max, exclude):
          if number not in exclude]
     )
 
-#generate 8 unique "random" numbers in a "random" order
+
+#generate 9 unique "random" numbers in a "random" order
 def create_random_puzzle_values():
     values = []
     for i in range(min_value, max_value + 1): #+1 in order to create 8 "random" values
--- eight_puzzle/puzzle_functions.py
+++ eight_puzzle/puzzle_functions.py
@@ -11,3 +11,4 @@ def print_puzzle(puzzle):
         print("")
 
 
+
