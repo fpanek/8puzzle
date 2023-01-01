3447e5b44a61950eb0daf5b8ee79f11276c87e9f
Florian Panek <florian.panek@outlook.com>
2023-01-01 19:05:56 +0100

    *)corrected description

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ from eight_puzzle.create_puzzle import  *
 from eight_puzzle.puzzle_functions import *
 
 #definition of puzzle - 2d array
-#     0 1 2 (col Axis - second value of array)
+#     0 1 2 (col Axis - first value of array)
 #  0 |0|1|2|
 #  1 |3|4|5|
 #  2 |6|7|8|
-#  (row Axis first value of the array)
+#  (row Axis second value of the array)
 if __name__ == '__main__':
     #puzzle = create_random_puzzle()
-
     #Test pattern:
     puzzle = [ [0,8,2], [3,7,5], [6,4,1]] #hammming distance = 4, manhattan distance = 8
     print_puzzle(puzzle)
