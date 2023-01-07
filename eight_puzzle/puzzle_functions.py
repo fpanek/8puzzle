c8380f10fae3a6e66d559fcedacbcf8b397a2061
Florian Panek <florian.panek@outlook.com>
2023-01-07 14:08:54 +0100

    *)working on implementation of algorithm

--- eight_puzzle/puzzle_functions.py
+++ eight_puzzle/puzzle_functions.py
@@ -7,6 +7,9 @@ debug = 1
 
 import math as Math
 import copy
+from eight_puzzle.priority_queue import *
+from eight_puzzle.puzzle_node import *
+
 
 from heapq import heappush, heappop
 
@@ -27,34 +30,117 @@ def print_puzzle(puzzle):
 #solve puzzle
 # required Parameters: puzzle (2d array), method to solve (hamming/manhattan = h or m),
 # returns: solved puzzle, number of expanded nodes, solve time
-def solve_puzzle(puzzle, method):
+def solve_puzzle(puzzle,puzzle_goal, method):
+    #TODO check for solvability e.g if function is used to solve non solvable puzzle
+    openList = PriorityQueue()
+    node = puzzle_node(puzzle, 0, 0, 0)
+    openList.push(node,0)
+    closedList = []
+
+    while not openList.isEmpty():
+        #puzzle = openList.pop().puzzle
+        #print(puzzle)
+        #if puzzle == puzzle_goal:
+        #    return puzzle
+        #closedList.append(puzzle)
+
+        current_puzzle = openList.pop().puzzle
+        if current_puzzle == puzzle_goal:
+            return current_puzzle
+
+        #check possible moves
+        #moves are 1=left,2=up,3=right,4=down
+        possible_moves = check_possible_moves(current_puzzle)
+
+        #execute possible moves
+        for i in possible_moves:
+            print("test")
+            try_move = move_empty_tile(puzzle,i)
+            if try_move not in closedList:
+                closedList.append(try_move) #add new node to examined nodes
+                openList.push(puzzle_node(puzzle, 0, 0, 0), 0)#add new node to heap #TODO 0,0,0,0 set correct values h/g/n
+            else:
+                print("deleting node required?")
+
+        #calculate heuristic distance for possible moves
+
+        #if the path to the neighbor is better than the previos one save it
+
+        #if the neighbor is not in openset add it to list
+
+
+
+
+
+#moves are 1=left,2=up,3=right,4=down
+def check_possible_moves(puzzle):
+    possible_moves = []
+    x,y = find_tile_position(puzzle, 0)
+    #check for left
+    if y>= 1 and y<=2:
+        #move left possible
+        possible_moves.append(1)
+    #check for up:
+    if x>= 1 and x<=2:
+        #move up possible
+        possible_moves.append(2)
+    #check for right:
+    if y<=1 and y>=0:
+        #move right possible
+        possible_moves.append(3)
+    #check for down:
+    if x<=1 and x>=0:
+        #move down possible
+        possible_moves.append(4)
+    return possible_moves
+
+
+#find tile (e.g. 0 for empty tile) and return position
+def find_tile_position(puzzle,tile_value):
+    xFound = 0
+    yFound = 0
+    for x in range(3):
+        for y in range(3):
+            #print("current value:", puzzle[x][y])
+            if puzzle[x][y] == tile_value:
+                xFound = x
+                yFound = y
+    return xFound, yFound
+
+
+
+#TODO
+#moves are 1=left,2=up,3=right,4=down
+def move_empty_tile(puzzle, direction):
+    print("todo")
+    new_puzzle = copy.deepcopy(puzzle)
+    x,y = find_tile_position(new_puzzle, 0)
+    match direction:
+        case 1:
+            new_tile = new_puzzle[x][y-1]
+            new_puzzle[x][y - 1] = 0
+        case 2:
+            new_tile = new_puzzle[x+1][y]
+            new_puzzle[x][y - 1] = 0
+        case 3:
+            new_tile = new_puzzle[x][y+1]
+            new_puzzle[x][y - 1] = 0
+        case 4:
+            new_tile = new_puzzle[x-1][y]
+            new_puzzle[x][y - 1] = 0
+    new_puzzle[x][y] = new_tile
+    return new_puzzle
+
+#calculates the heuristic distance
+#puzzle = 8 puzzle 2d array
+#method = h or m (hamming or manhattan)
+def calculate_heuristic_distance(puzzle, method):
     match method:
         case "h":
             heuristic_distance = calculate_hamming_distance(puzzle)
         case "m":
             heuristic_distance = calculate_manhattan_distance(puzzle)
-    if debug:
-        print("Selected heuristic: ", method, "calculated distance: ", heuristic_distance)
-    #return puzzle, expanded_nodes, solve_time
-
-def make_move(direction, position1, position2):
-    print("todo")
-
-def solve_puzzle_hamming():
-    print("todo")
-
-
-def solve_puzzle_manhattan():
-    print("todo")
-
-
-#TODO
-def move_is_valid(puzzle):
-    print("todo")
-
-#TODO
-def move_tile(puzzle):
-    print("todo")
+    return heuristic_distance
 
 def calculate_manhattan_distance(puzzle):
     finalStatePuzzle = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
--- main.py
+++ main.py
@@ -21,19 +21,24 @@ if __name__ == '__main__':
     #print("manhattan distance of the given puzzle is: ", manhattan_distance)
     #solve_puzzle(puzzle, "m")
     #node = puzzle_node(puzzle, 3, 3, 2000)
+    #puzzle1 = [ [1,1,2], [1,7,5], [1,4,1]] #hammming distance = 4, manhattan distance = 8
+    #puzzle2 = [ [2,2,2], [2,7,5], [2,6,1]] #hammming distance = 4, manhattan distance = 8
+    #puzzle3 = [ [3,3,2], [3,3,5], [3,4,1]] #hammming distance = 4, manhattan distance = 8
+    #prioqueue  = PriorityQueue()
+    #prioqueue.push(puzzle1,3)
+    #prioqueue.push(puzzle2,2)
+    #prioqueue.push(puzzle3,1)
+    #print_puzzle(prioqueue.pop())
 
-    puzzle1 = [ [1,1,2], [3,7,5], [6,4,1]] #hammming distance = 4, manhattan distance = 8
-    puzzle2 = [ [2,2,2], [3,7,5], [4,6,1]] #hammming distance = 4, manhattan distance = 8
-    puzzle3 = [ [3,3,2], [7,3,5], [6,4,1]] #hammming distance = 4, manhattan distance = 8
-    prioqueue  = PriorityQueue()
-    prioqueue.push(puzzle1,900)
-    prioqueue.push(puzzle2,200)
-    prioqueue.push(puzzle3,200)
-    print_puzzle(prioqueue.pop())
+    #print ("printing queue")
+    #while not prioqueue.isEmpty():
+    #    state = prioqueue.pop()
+    #    print_puzzle(state)
 
-    print ("printing queue")
-    while not prioqueue.isEmpty():
-        state = prioqueue.pop()
-        print_puzzle(state)
+    puzzle1 = [ [1,9,2], [2,7,0], [1,4,8]] #hammming distance = 4, manhattan distance = 8
+    puzzle_goal = [ [1,1,2], [1,7,5], [1,4,1]] #hammming distance = 4, manhattan distance = 8
+    #puzzle_goal = [ [1,9,2], [2,7,0], [1,4,8]] #hammming distance = 4, manhattan distance = 8
+
+    solve_puzzle(puzzle1, puzzle_goal, "h")
 
 
